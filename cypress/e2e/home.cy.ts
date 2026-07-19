0041262cd9afe2cb27e87f0b7d8f08d658950902
Mritunjay Kumar <mk1632003@gmail.com>
2026-07-19 23:59:47 +0530

    chore: remove Cypress testing suite
    Delete Cypress configuration, workflow, and tests to reduce repository bloat.

--- cypress/e2e/home.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("Home Page", () => {
-  it("loads successfully", () => {
-    cy.visit("http://localhost:3000");
-
-    cy.get("body").should("be.visible");
-    cy.title().should("not.be.empty");
-  });
-});
