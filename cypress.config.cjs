5cba7e45f5c5d6f7f49dcd023540e1caa5c1fcc7
Mritunjay Kumar <mk1632003@gmail.com>
2026-07-19 21:38:29 +0530

    Disable support file in Cypress config
    Set supportFile to false in Cypress configuration.

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   e2e: {
     baseUrl: 'http://localhost:3000',
+    // FIX: Tells Cypress it's perfectly fine that we don't have a support file yet
+    supportFile: false, 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
